1e8964c0cf996bf3d9484f4ad91bdcc63353f970
pranavnan <pranavnan612@gmail.com>
2023-02-12 23:43:04 +0530

    useNavigate hook

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,16 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 function Home() {
+  const navigate = useNavigate();
+  function navigatehandler() {
+    navigate("/products");
+  }
   return (
     <>
       <h1>My Home Page</h1>
       <Link to="/products">Products List</Link>
+      <button onClick={navigatehandler}>Navigate</button>
     </>
   );
 }
