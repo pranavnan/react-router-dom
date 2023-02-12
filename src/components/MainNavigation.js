37244ff229d49909795c9c96c49df3e7e62dfd6b
pranavnan <pranavnan612@gmail.com>
2023-02-12 23:37:58 +0530

    till NavLinks

--- src/components/MainNavigation.js
+++ src/components/MainNavigation.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 import "./MainNavigation.css";
 
 function MainNavigation() {
@@ -8,10 +8,23 @@ function MainNavigation() {
       <nav>
         <ul className="list">
           <li>
-            <Link to="/">Home</Link>
+            <NavLink
+              className={({ isActive }) => (isActive ? "active" : "")}
+              to="/"
+              // style={({isActive}) => }
+              end
+              // One of the properties you can pass to the NavLink component is end, which determines whether the active state should be applied only when the link is at the end of the URL path. By default, the active state is applied to the link whenever the URL path matches the to property of the NavLink.
+            >
+              Home
+            </NavLink>
           </li>
           <li>
-            <Link to="/products">Products</Link>
+            <NavLink
+              className={({ isActive }) => (isActive ? "active" : "")}
+              to="/products"
+            >
+              Products
+            </NavLink>
           </li>
         </ul>
       </nav>
